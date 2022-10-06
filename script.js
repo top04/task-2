c9758c1ab21096b5d14b9c0f168ad161c8520750
top_004 <locinbekerkinov38@gmail.com>
2022-10-06 20:02:02 +0500

    none

---
+++ script.js
@@ -0,0 +1,9 @@
+var x = +prompt("Введите первое число");
+var y = +prompt("Введите первое число");
+var z = +prompt("Введите первое число");
+var res = (x + y + z) / 3;
+alert(res);
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(res);
